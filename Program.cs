38490fb610fcfa36a3a1e0ff4bbff7e9dee28044
stanton101 <stanton.fredericks@praesignis.co.za>
2022-01-05 07:17:47 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace Cproject
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+            string fname = "Stanton";
+            string lname = "Fredericks";
+			Console.WriteLine("Your full name is "+fname+" "+lname);
+		}
+	}
+}
